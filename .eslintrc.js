fc25cef057cd5899f985b20d895db070de257118
Twittytop <hongwei.tu@chukong-inc.com>
2019-03-18 19:49:24 +0800

    chore(eslint): 增加代码检查配置

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/essential",
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    "standard"
+  ],
+  // required to lint *.vue files
+  plugins: ["vue"],
+  // add your custom rules here
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    // 块语句前面加空格
+    "space-before-blocks": ["warn", "always"],
+    // 对象换行
+    "object-curly-newline": ["warn", "always"],
+    "vue/jsx-uses-vars": 2,
+    // allow async-await
+    "generator-star-spacing": "off",
+    // allow console during development
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  }
+};
