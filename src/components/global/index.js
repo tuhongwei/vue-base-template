6e2c50daaae002ba238e25c38d6449a2c937dd9d
Twittytop <hongwei.tu@chukong-inc.com>
2019-03-20 18:28:02 +0800

    fix(global): fix bug

---
+++ src/components/global/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+const requireComponent = require.context('.', true, /\.(vue|js)$/);
+requireComponent.keys().forEach(fileName => {
+  if (fileName === './index.js') return;
+  // 获取组件配置
+  const componentConfig = requireComponent(fileName);
+  const componentEntity = componentConfig.default || componentConfig;
+  Vue.component(componentEntity.name, componentEntity);
+});
