2bfe713ee7fe87bc4057e30d8574cdc336c4de3e
Twittytop <hongwei.tu@chukong-inc.com>
2019-03-21 10:53:32 +0800

    chore(.stylelintrc.js): 修改stylelint配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   // https://github.com/stylelint/stylelint-config-standard/blob/master/index.js
   extends: "stylelint-config-standard",
   rules: {
-
+    // always be whitespace before the commas in multi-line selector lists
+    "selector-list-comma-newline-before": "always-multi-line",
+    // never be a newline after the commas in multi-line selector lists
+    "selector-list-comma-newline-after": "never-multi-line",
+    // always be a single space after the commas in single-line selector lists
+    "selector-list-comma-space-after": "always-single-line"
   }
 };
