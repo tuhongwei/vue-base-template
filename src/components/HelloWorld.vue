133a3f119870e0f7aab5d86f4f2a1b97d25e74e6
Twittytop <hongwei.tu@chukong-inc.com>
2019-03-21 14:22:25 +0800

    chore(HelloWorld.vue): prettier和stylelint  selector-list-comma-newline-before冲突

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,7 +24,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,h2 {
+h1,
+h2 {
   font-size: 0.14rem;
   font-weight: normal;
   text-align: center;
