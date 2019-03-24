6ef9bcdd2128b1b1ddfb346d31f98de56c2a8ad0
李晓云 <lixiaoyun@lixiaoyundeMacBook-Pro.local>
2019-03-24 23:38:58 +0800

    chore(errorCapture): 增加错误捕获

---
+++ src/errorCapture.js
@@ -0,0 +1,5 @@
+// 捕获错误并上传
+import Vue from 'vue';
+Vue.config.errorHandler = (err, vm, info) => {
+	
+};
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 import store from './stores';
 import './global';
 import './interception';
+import './errorCapture'
 
 Vue.config.productionTip = false;
 
