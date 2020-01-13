738fb2cf4e7d7ad61f2e97c5a89ad668f9ac3062
Twittytop <hongwei.tu@chukong-inc.com>
2020-01-13 17:02:02 +0800

    feat(*): 加上骨架屏

--- src/skeleton/entry-skeleton.js
+++ src/skeleton/entry-skeleton.js
@@ -5,7 +5,7 @@ export default new Vue({
   components: { Skeleton2 },
   template: `
   <div>
-    <skeleton2 id="skeleton2" />
+    <skeleton2 id="skeleton2" style="display:none"/>
   </div>
   `
 });
